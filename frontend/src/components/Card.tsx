c6e1a63847b3647cf3499a7396967733238943ad
Vitaliy-colab <vitaliyzaritskuy@gmail.com>
2025-10-15 20:08:29 +0300

    Functions: CardDescription, CardAction, CardContent, CardFooter

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -45,3 +45,46 @@ export function CardTitle({ className, ...props }: CardSlotProps) {
 		/>
 	)
 }
+
+export function CardDescription({ className, ...props }: CardSlotProps) {
+	return (
+		<div
+			data-slot='card-description'
+			className={cn('text-gray-500 text-sm', className)}
+			{...props}
+		/>
+	)
+}
+
+export function CardAction({ className, ...props }: CardSlotProps) {
+	return (
+		<div
+			data-slot='card-action'
+			className={cn(
+				'col-start-2 row-span-2 row-start-1 self-start justify-self-end',
+				className
+			)}
+			{...props}
+		/>
+	)
+}
+
+export function CardContent({ className, ...props }: CardSlotProps) {
+	return (
+		<div
+			data-slot='card-content'
+			className={cn('px-6', className)}
+			{...props}
+		/>
+	)
+}
+
+export function CardFooter({ className, ...props }: CardSlotProps) {
+	return (
+		<div
+			data-slot='card-footer'
+			className={cn('flex items-center px-6 pt-6 border-t', className)}
+			{...props}
+		/>
+	)
+}
