22988fc7449b77a44dfcc40f544ecaff6194acf2
NZR <nzr@gmail.com>
2025-10-19 18:30:00 +0300

    MongoDBService

---
+++ backend/Services/MongoDbService.cs
@@ -0,0 +1,23 @@
+ using backend.Config;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace backend.Services
+{
+    public class MongoDbService
+    {
+        private readonly IMongoDatabase _database;
+
+        public MongoDbService(IOptions<MongoDbSettings> options)
+        {
+            var client = new MongoClient(options.Value.ConnectionString);
+            _database = client.GetDatabase(options.Value.DatabaseName);
+        }
+
+        // Отримати будь-яку колекцію
+        public IMongoCollection<T> GetCollection<T>(string collectionName)
+        {
+            return _database.GetCollection<T>(collectionName);
+        }
+    }
+}
